ef1ea43aefdbe7da9d589351863ce659d68140d4
Igor <ivoivoivo@mail.ru>
2022-09-21 00:06:54 +0300

    fifth commit

--- endex.js
+++ endex.js
@@ -158,7 +158,23 @@ let fccRegex = /freeCodeCamp/i; // Change this line
 //Extract Matches
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
-let result = extractStr.match(codingRegex); // Change this line
+//let result = extractStr.match(codingRegex); // Change this line
+
+//Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+//let result = twinkleStar.match(starRegex); // Change this line
+
+//Match Anything with Wildcard Period
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /un./; // Change this line
+//let result = unRegex.test(exampleStr);
+
+//Match Single Character with Multiple Possibilities
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+//let result = quoteSample.match(vowelRegex); // Change this line
+
 
 
 
