3dbcf3530a947bf384412330c509422e94e22e0d
Igor <ivoivoivo@mail.ru>
2022-10-02 22:24:41 +0300

    seventh commit

--- endex.js
+++ endex.js
@@ -227,6 +227,54 @@ let hello = "   Hello, World!  ";
 let wsRegex = /^\s+|\s+$/g; // Change this line
 //let result = hello.replace(wsRegex, ""); // Change this line
 
+//Access an Array's Contents Using Bracket Notation
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "i";
+console.log(myArray);
+
+//Add Items to an Array with push() and unshift()
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift("I", 2, "three",);
+    arr.push(7, 'VIII', 9)
+    // Only change code above this line
+    return arr;
+}
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+//Remove Items from an Array with pop() and shift()
+function popShift(arr) {
+    let popped = arr.pop(); // Change this line
+    let shifted = arr.shift(); // Change this line
+    return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+//Remove Items Using splice()
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+
+let newArr = arr.splice(1, 4);
+
+console.log(arr);
+
+//Add Items Using splice()
+function htmlColorNames(arr) {
+
+    arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond")
+
+    return arr;
+}
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+//Copy Array Items Using slice()
+function forecast(arr) {
+
+    let newArr = arr.slice(2, 4);
+    return newArr;
+}
+
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
 
 
